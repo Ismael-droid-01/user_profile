6affc6050a5123112cfd376f71e31d53ee40998f
Samuel Gomez <gocs020101@gs.utm.mx>
2025-08-10 21:41:06 -0600

    fact: Text en la interfaz del etiquetador para mostrar el post completo

--- gui/labeler.py
+++ gui/labeler.py
@@ -2,7 +2,21 @@ import tkinter as tk
 from tkinter import ttk
 from utils.preprocessing import normalize_text
 
+def create_text_viewer(parent, content):
+    frame = tk.Frame(parent)
+    text_widget = tk.Text(frame, wrap="word", height=10, font=("Arial", 10))
+    text_widget.insert("1.0", content)
+    text_widget.config(state="disabled")  # solo lectura
+    scrollbar = tk.Scrollbar(frame, command=text_widget.yview)
+    text_widget.config(yscrollcommand=scrollbar.set)
+
+    text_widget.pack(side="left", fill="both", expand=True)
+    scrollbar.pack(side="right", fill="y")
+    return frame
+
+
 def show_viewer(user_vector, parent=None):
+    print(user_vector)
     view = tk.Toplevel(parent)
     view.title(user_vector["user_id"])
     view.geometry("400x800")
@@ -11,8 +25,8 @@ def show_viewer(user_vector, parent=None):
     # Texto original
     lbl_original_title = tk.Label(view, text="Texto original:", font=("Arial", 10, "bold"))
     lbl_original_title.pack(anchor="w", padx=10, pady=(10,0))
-    lbl_original_text = tk.Label(view, text=user_vector["text"], font=("Arial", 10))
-    lbl_original_text.pack(anchor="w", padx=20, pady=(0,10))
+    original_text_frame = create_text_viewer(view, user_vector["text"])
+    original_text_frame.pack(fill="both", padx=20, pady=(0, 10), expand=False)
 
     # Normalizar el texto
     clean_text, percentage_change = normalize_text(user_vector["text"])
@@ -20,8 +34,8 @@ def show_viewer(user_vector, parent=None):
     # Texto limpio
     lbl_clean_title = tk.Label(view, text="Texto limpio:", font=("Arial", 10, "bold"))
     lbl_clean_title.pack(anchor="w", padx=10)
-    lbl_clean_text = tk.Label(view, text=clean_text, font=("Arial", 10))
-    lbl_clean_text.pack(anchor="w", padx=20, pady=(0,10))
+    clean_text_frame = create_text_viewer(view, clean_text)
+    clean_text_frame.pack(fill="both", padx=20, pady=(0, 10), expand=False)
 
     # Porcentaje cambios
     lbl_changes = tk.Label(view, text=f"% cambios: {percentage_change} %", font=("Arial", 10, "bold"))
